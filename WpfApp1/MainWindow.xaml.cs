c84cc1951eb1d7ab013e6f0b965211e00a3ef18e
Vlada Glushkova <lebedia@sfedu.ru>
2025-03-17 14:57:11 +0300

    2

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -11,14 +11,75 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        Window1 workWindow = new Window1();
+        bool Choised = false;
+        private void bRgistration_Click(object sender, RoutedEventArgs
+e)
         {
-            InitializeComponent();
+            Choised = false;
+            tbLogin.IsEnabled = true;
+            tbPass1.IsEnabled = true;
+            tbPass2.IsEnabled = true;
+            bEnter.IsEnabled = true;
         }
+        private void bLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Choised = true;
+            tbLogin.IsEnabled = true;
+            tbPass1.IsEnabled = true;
+            tbPass2.IsEnabled = false;
+            bEnter.IsEnabled = true;
+        }
+
+
+        private void Registration(string tbL, string tbP1, string tbP2)
+        {
+            if (tbL == "")
+            {
+                MessageBox.Show("Введите логин");
+                return;
+            }
+            if (tbP1 == tbP2)
+            {
+            }
+            else
+            {
+                MessageBox.Show("Пароли различны");
+                return;
+            }
+
+            workWindow.lUser.Content = "Текущий пользователь: " + tbL;
+            workWindow.Show();
+            Close();
+        }
+            private void Logining(string tbL, string tbP1)
+        {
+            if (tbL == "")
+            {
+                MessageBox.Show("Введите логин");
+                return;
+            }
+            workWindow.lUser.Content = "Текущий пользователь: "+tbL; 
+            workWindow.Show();
+            Close();
+                
+            }
+        private void bEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (Choised == false)
+            {
+                Registration(tbLogin.Text, tbPass1.Text,
+                tbPass2.Text);
+            }
+            else
+            {
+                Logining(tbLogin.Text, tbPass1.Text);
+            }
+        }
+
+        
     }
+
 }
--- WpfApp1/Window1.xaml.cs
+++ WpfApp1/Window1.xaml.cs
@@ -11,17 +11,71 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для Window1.xaml
-    /// </summary>
+    
     public partial class Window1 : Window
     {
         public Window1()
         {
             InitializeComponent();
+        }
+
+        public class PositionObject
+        {
+            public int PositionID { get; set; }
+            public string PositionName { get; set; }
+            public PositionType PositionType { get; set; }
+            public int PositionValue { get; set; }
+            public double PositionPrice { get; set; }
+            public double PriceCurrency { get; set; }
+        }
+
+        public enum PositionType
+        {
+            Production = 0,
+            Service = 1,
+            Bonus = 2
+        }
+
+        private void Window_loaded(object sender, RoutedEventArgs e)
+        {
+            cbPosType.ItemsSource =
+            Enum.GetValues(typeof(PositionType));
+            cbPosType.SelectedIndex = 0;
+        }
+
+        private void bQuotes_Click(object sender, RoutedEventArgs e)
+        {
+            lbQuotes.Items.Clear();
+            lbQuotes.Items.Add("75,47 USD");
+            lbQuotes.Items.Add("80,24 EUR");
+            lbQuotes.Items.Add("10,88 CNY");
+            lbQuotes.SelectedIndex = 0;
+        }
+
+        private void bAdd_Click(object sender, RoutedEventArgs e)
+        {
+            PositionObject item = new PositionObject();
+            item.PositionID = dgMain.Items.Count + 1;
+            item.PositionName = tbPosName.Text;
+            item.PositionType =
+            (PositionType)cbPosType.SelectedItem;
+            item.PositionValue = int.Parse(tbPosValue.Text);
+            item.PositionPrice = double.Parse(tbPosPrice.Text);
+            string[] strings =
+            lbQuotes.SelectedItem.ToString().Split(' ');
+            item.PriceCurrency =
+            Math.Round(double.Parse(tbPosPrice.Text) /
+            double.Parse(strings[0]), 2);
+            dgMain.Items.Add(item);
+        }
+
+        private void sMarkup_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lMarkup.Content = Math.Round(100 + sMarkup.Value, 2) + "%";
         }
     }
 }
